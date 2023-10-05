7b108d86f36200ef8d39d0008413909b86f9939b
emmanuel <emmvazquez@gmail.com>
2023-10-05 12:26:39 -0600

    editar v1.1

--- app/Views/alumno/editar.php
+++ app/Views/alumno/editar.php
@@ -1,26 +1,33 @@
 <div class="container">
     <div class="row">
     <?php print_r($alumno); ?>
+    <?php echo $alumno->nombre ?>
         <div class="col-8">
             <h2>Editar Alumno</h2>
             <form action="<?= base_url('alumno/update'); ?>" method="POST">
             <?= csrf_field() ?>
                 <div class="mb-3">
                     <label for="nombre" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" name="nombre" id="nombre">
+                    <input type="text" class="form-control" 
+                    name="nombre" id="nombre" value="<?= $alumno->nombre ?>">
                 </div>
                 
                 <div class="mb-3">
                     <label for="fechaNacimiento" class="form-label">Fecha de nacimiento </label>
                     <input type="date" class="form-control" name="fechaNacimiento" 
-                    id="fechaNAcimiento">
+                    id="fechaNAcimiento" value="<?= $alumno->fechaNacimiento ?>">
                 </div>
 
                 <div class="mb-3">
                     <label for="sexo">Sexo</label>
                     <select name="sexo" id="sexo" class="form-control">
-                        <option value="Hombre">Hombre</option>
-                        <option value="Mujer">Mujer</option>
+                        <?php if($alumno->sexo == "HOMBRE"): ?>
+                        <option value="Hombre" selected>Hombre</option>
+                        <option value="Mujer" >Mujer</option>
+                        <?php else: ?>
+                        <option value="Hombre" >Hombre</option>
+                        <option value="Mujer" selected>Mujer</option>
+                        <?php endif ?>
                     </select>
                 </div>
 
